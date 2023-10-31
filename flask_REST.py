7746fc1692e0ffb5a00fd798040d6b54386ad397
Francesco Vaselli <francesco.vaselli@protonmail.com>
2023-10-31 14:32:08 +0100

    remove type req

--- flask_REST.py
+++ flask_REST.py
@@ -22,7 +22,7 @@ class CustomJSONProvider(JSONProvider):
     def dumps(self, obj, **kwargs):
         return json.dumps(obj, **kwargs, cls=CustomJSONEncoder)
     
-    def loads(self, s: str | bytes, **kwargs):
+    def loads(self, s, **kwargs):
         return json.loads(s, **kwargs)
     
 app = Flask(__name__)
