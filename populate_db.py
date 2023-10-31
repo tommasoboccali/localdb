339e51cbbaef0ff9929e64b05dff0a8ce58350a7
Francesco Vaselli <francesco.vaselli@protonmail.com>
2023-10-31 10:46:06 +0100

    create objectid ref and timeit

--- populate_db.py
+++ populate_db.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 from bson import json_util
 from jsonschema import validate, ValidationError
 import pymongo
@@ -7,6 +8,7 @@ import json
 from dotenv import load_dotenv
 import random
 from faker import Faker
+import timeit
 
 fake = Faker()
 
@@ -47,34 +49,66 @@ tests_collection.drop()
 
 # Populate modules
 module_ids = []
-for i in range(1000):  # Creating 100 random modules
+for i in range(2000):  # Creating 100 random modules
     module = {
         "moduleID": f"M{i}",
         "position": random.choice(["lab", "field", "storage"]),
+        "logbook": {"entry": "Initial setup"},
+        "local_logbook": {"entry": "Local setup"},
+        "ref_to_global_logbook": [],
         "status": random.choice(["operational", "maintenance", "decommissioned"]),
-        "overall_grade": random.choice(["A", "B", "C"])
+        "overall_grade": random.choice(["A", "B", "C"]),
+        "underwent_tests": []
     }
-    modules_collection.insert_one(module)
-    module_ids.append(module['moduleID'])
+    inserted_module = modules_collection.insert_one(module)
+    module_ids.append(inserted_module.inserted_id)
 
 # Populate tests
-for i in range(500):  # Creating 50 random tests
+test_ids = []
+for i in range(10000):  # Creating 50 random tests
+    involved_modules = random.sample(module_ids, k=random.randint(1, 10))
     test = {
         "testID": f"T{i}",
-        "modules_list": random.sample(module_ids, k=random.randint(1, 10)),  # 1 to 10 modules per test
+        "modules_list": involved_modules,
         "testType": random.choice(["Type1", "Type2", "Type3"]),
         "testDate": fake.date(),
         "testOperator": fake.name(),
         "testStatus": random.choice(["completed", "ongoing", "failed"]),
         "testResults": {"result": random.choice(["pass", "fail"])}
     }
-    tests_collection.insert_one(test)
+    inserted_test = tests_collection.insert_one(test)
+    test_ids.append(inserted_test.inserted_id)
+
+    # Update modules' underwent_tests field
+    for module_id in involved_modules:
+        modules_collection.update_one(
+            {"_id": ObjectId(module_id)},
+            {"$push": {"underwent_tests": inserted_test.inserted_id}}
+        )
+
+def fetch_tests_for_module():
+    module_id_to_query = random.choice(module_ids)
+    module_data = modules_collection.find_one({"_id": module_id_to_query})
+    tests_module_underwent = tests_collection.find({"_id": {"$in": module_data.get("underwent_tests", [])}}).count()
+
+def fetch_modules_for_test():
+    test_id_to_query = random.choice(test_ids)
+    test_data = tests_collection.find_one({"_id": test_id_to_query})
+    modules_in_test = modules_collection.find({"_id": {"$in": test_data.get("modules_list", [])}}).count()
+
+# Measure the time taken for fetching all tests for a random module
+time_for_fetching_tests = timeit.timeit(fetch_tests_for_module, number=100)
+print(f"Time taken for fetching all tests for a module over 100 iterations: {time_for_fetching_tests} seconds")
+
+# Measure the time taken for fetching all modules for a random test
+time_for_fetching_modules = timeit.timeit(fetch_modules_for_test, number=100)
+print(f"Time taken for fetching all modules for a test over 100 iterations: {time_for_fetching_modules} seconds")
 
 # print 10 random entries to show that the database is populated
-print("Modules:")
-for module in modules_collection.aggregate([{"$sample": {"size": 10}}]):
-    print(json.dumps(module, indent=2, default=json_util.default))
+# print("Modules:")
+# for module in modules_collection.aggregate([{"$sample": {"size": 10}}]):
+#     print(json.dumps(module, indent=2, default=json_util.default))
 
-print("Tests:")
-for test in tests_collection.aggregate([{"$sample": {"size": 10}}]):
-    print(json.dumps(test, indent=2, default=json_util.default))
+# print("Tests:")
+# for test in tests_collection.aggregate([{"$sample": {"size": 10}}]):
+#     print(json.dumps(test, indent=2, default=json_util.default))
