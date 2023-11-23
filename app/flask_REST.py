d8e25aa79f76694a76033dee3b997f34ecdc9bfb
Francesco Vaselli <francesco.vaselli@protonmail.com>
2023-11-23 16:58:41 +0100

    cabling snapshot

--- app/flask_REST.py
+++ app/flask_REST.py
@@ -379,6 +379,53 @@ class CablesResource(Resource):
 
 api.add_resource(CablesResource, "/cables", "/cables/<string:name>")
 
+# a route for crates for now equal to cables
+class CratesResource(Resource):
+    def get(self, name=None):
+        if name:
+            entry = crates_collection.find_one({"name": name})
+            if entry:
+                entry["_id"] = str(entry["_id"])  # convert ObjectId to string
+                return jsonify(entry)
+            else:
+                return {"message": "Entry not found"}, 404
+        else:
+            entries = list(crates_collection.find())
+            for entry in entries:
+                entry["_id"] = str(entry["_id"])
+            return jsonify(entries)
+
+    def post(self):
+        try:
+            new_entry = request.get_json()
+            validate(instance=new_entry, schema=cables_schema)
+            crates_collection.insert_one(new_entry)
+            return {"message": "Entry inserted"}, 201
+        except ValidationError as e:
+            return {"message": str(e)}, 400
+
+    def put(self, name):
+        if name:
+            updated_data = request.get_json()
+            crates_collection.update_one({"name": name}, {"$set": updated_data})
+            return {"message": "Entry updated"}, 200
+        else:
+            return {"message": "Entry not found"}, 404
+
+    def delete(self, name):
+        if name:
+            entry = crates_collection.find_one({"name": name})
+            if entry:
+                crates_collection.delete_one({"name": name})
+                return {"message": "Entry deleted"}, 200
+            else:
+                return {"message": "Entry not found"}, 404
+        else:
+            return {"message": "Entry not found"}, 404
+        
+    
+api.add_resource(CratesResource, "/crates", "/crates/<string:name>")
+
 # Define the schema for validation
 
 class CableTemplatesResource(Resource):
