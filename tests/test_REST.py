4cb3ae03860764711cd7ecfc7f5c1d3379acb2cd
Francesco Vaselli <francesco.vaselli@protonmail.com>
2023-11-24 11:04:24 +0100

    cabling snapshot from crate

--- tests/test_REST.py
+++ tests/test_REST.py
@@ -25,6 +25,7 @@ class TestAPI(TestCase):
         db.tests.drop()
         db.cables.drop()
         db.cables_templates.drop()
+        db.crates.drop()
 
     def tearDown(self):
         db.modules.drop()
@@ -33,6 +34,7 @@ class TestAPI(TestCase):
         db.tests.drop()
         db.cables.drop()
         db.cables_templates.drop()
+        db.crates.drop()
 
     def test_fetch_all_modules_empty(self):
         response = self.client.get("/modules")
