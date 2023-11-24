d3f5feb67a2b547c1ded987547b178d500573b16
Francesco Vaselli <francesco.vaselli@protonmail.com>
2023-11-24 16:41:34 +0100

    final cabling map tests

--- app/flask_REST.py
+++ app/flask_REST.py
@@ -812,7 +812,6 @@ def traverse_cables(starting_point_name, starting_cable, starting_side, starting
             (ct for ct in cable_templates if ct["type"] == next_cable["type"]), None
         )
         if starting_side == "detSide":
-            print(next_port)
             next_port = int(cable_template["internalRouting"].get(str(next_port), None))
         else:
             next_port = int(
@@ -829,9 +828,6 @@ def traverse_cables(starting_point_name, starting_cable, starting_side, starting
             )
         if not next_port:
             break
-        print(next_cable[other_side])
-        for conn in next_cable[other_side]:
-            print(conn["port"], type(conn["port"]), next_port, type(next_port))
 
         next_cable_id = next(
             (
